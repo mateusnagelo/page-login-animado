b4f205eb164fe5a47af1dd986959cde1790e5adb
mateusnagelo <mateusgenius1234@hotmail.com>
2022-07-09 15:54:45 -0300

    inclusao da base js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,30 @@
+function logar() {
 
+    var login = document.getElementById('login').value;
+    var senha = document.getElementById('senha').value;
+
+
+    if (login == "suporte" && senha == "1") {
+        // alert('Sucesso');
+
+        location.href = "https://mcnsistemas.netlify.app/";
+
+    } else {
+        alert('Usuario ou senha incorretos')
+    }
+
+}
+
+document.addEventListener("keypress", function(e) {
+
+    if (e.key === "Enter") {
+
+
+        const btn = document.querySelector("#send");
+
+        btn.click();
+
+
+    }
+
+});
